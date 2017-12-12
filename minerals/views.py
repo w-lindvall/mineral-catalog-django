0fb8c838cffa132dcc266297827008ffeeb73c61
w-lindvall <32217534+w-lindvall@users.noreply.github.com>
2017-12-12 18:21:37 +0100

    Update views.py

--- minerals/views.py
+++ minerals/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Mineral
 
-# Create your views here.
+
 def home_page(request):
-    '''View displays list of mineral names with links to individual detail page'''
+    """View displays list of mineral names with links"""
     minerals = Mineral.objects.all()
     return render(request, 'index.html', {'minerals': minerals})
 
+
 def detail(request, pk):
-    '''View displays individual mineral details'''
+    """View displays individual mineral details"""
     mineral = get_object_or_404(Mineral, pk=pk)
     return render(request, 'detail.html', {'mineral': mineral})
